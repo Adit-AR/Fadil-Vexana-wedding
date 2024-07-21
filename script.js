f3281931a7371693e472154283f404267f9d685d
unknown <adityaR929@outlook.com>
2024-07-21 17:04:21 +0700

    ganti vibes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const home = document.querySelector(".home button");
 let packaging = document.querySelector(".packaging");
 let nama = document.querySelector(".people-name");
+let undangan = ["fian", "azam", "fahmi", "ivan", "syharul"];
+let pr = "";
+
+let prm = function () {
+  pr = prompt("masukkan nama sesuai dengan undangan di WA");
+  return pr;
+};
 
 let inableScroll = function () {
   packaging.classList.add("muncul");
@@ -8,13 +15,28 @@ let inableScroll = function () {
 
 let scrol = function () {
   home.addEventListener("click", function () {
-    packaging.classList.remove("muncul");
-    let isi = prompt("masukkan nama sesuai yang di kririm melalui Whats up");
-    nama.innerHTML = isi;
-    localStorage.setItem("opened", "true");
+    prm();
+    nama.innerHTML = pr;
+    check();
   });
 };
 scrol();
+
+let check = function () {
+  for (let i = 0; i <= undangan.length; i++) {
+    if ((pr == undangan[i]) == true) {
+      packaging.classList.remove("muncul");
+      localStorage.setItem("opened", "true");
+      console.log(i);
+      return pr;
+    } else {
+      alert(
+        "Nama yang anda masukkan tidak ada dalam daftar undangan, pastikan sudah memasukkan nama sesuai yang di kirim di WA"
+      );
+      return pr;
+    }
+  }
+};
 
 if (!localStorage.getItem("opened")) {
   inableScroll();
